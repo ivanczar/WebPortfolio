32e4192021626b924113102ba17371493561759d
Ivan <ivanczar2013@gmail.com>
2022-06-29 10:50:24 +1200

    added infosection

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -71,7 +71,7 @@ export const SideBtnWrap = styled.div`
 
 export const SidebarRoute = styled(LinkR)`
   border-radius: 50px;
-  background: #01bf71;
+  background: #5865F2;
   white-space: nowrap;
   padding: 16px 64px;
   color: #010606;
@@ -85,6 +85,6 @@ export const SidebarRoute = styled(LinkR)`
   &:hover {
     transition: all 0.2s ease-in-out;
     background: #fff;
-    color: #01bf71;
+    color: #5865F2;
   }
 `;
